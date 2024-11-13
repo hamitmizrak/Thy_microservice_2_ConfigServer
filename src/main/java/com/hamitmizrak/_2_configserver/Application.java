22c5de97e9287b7df2c369892fb089efd5a84bd5
Hamit Mizrak <hamitmizrak@gmail.com>
2024-11-13 03:09:22 +0300

    Revize

--- src/main/java/com/hamitmizrak/_2_configserver/Application.java
+++ src/main/java/com/hamitmizrak/_2_configserver/Application.java
@@ -2,10 +2,12 @@ package com.hamitmizrak._2_configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 // Config Server Enable
 @EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 
