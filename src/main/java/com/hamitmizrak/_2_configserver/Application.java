fe974b0fd348546a74b2cf87893ad7c88227e867
Hamit Mizrak <hamitmizrak@gmail.com>
2024-11-13 22:24:40 +0300

    config server

--- src/main/java/com/hamitmizrak/_2_configserver/Application.java
+++ src/main/java/com/hamitmizrak/_2_configserver/Application.java
@@ -5,10 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+@SpringBootApplication
 // Config Server Enable
 @EnableConfigServer
-@EnableDiscoveryClient
-@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
